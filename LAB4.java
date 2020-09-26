803c61fefa3408dbec45785825027f9790bf72bf
Richy Twu <imrk85624@gmail.com>
2020-09-26 15:11:02 +0800

    init version

---
+++ LAB4.java
@@ -0,0 +1,16 @@
+public class LAB4 {
+	
+	public int add(int num1, int num2) {
+		return num1+num2;
+	}
+	public int minus(int num1, int num2) {
+		return num1-num2;
+	}
+	public static void main(String[] args) {
+		LAB4 test = new LAB4();
+		int num1 = 4;
+		int num2 = 2;
+		System.out.println(test.add(num1, num2));
+		System.out.println(test.minus(num1, num2));
+	}
+}
