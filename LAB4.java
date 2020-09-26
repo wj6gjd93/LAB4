9f9501b519f08223308489f235663adfaa2aba2b
Richy Twu <imrk85624@gmail.com>
2020-09-26 15:25:22 +0800

    float complete

--- LAB4.java
+++ LAB4.java
@@ -1,15 +1,15 @@
 public class LAB4 {
 	
-	public float add(int num1, int num2) {
+	public float add(float num1, float num2) {
 		return num1+num2;
 	}
-	public float minus(int num1, int num2) {
+	public float minus(float num1, float num2) {
 		return num1-num2;
 	}
 	public static void main(String[] args) {
 		LAB4 test = new LAB4();
-		int num1 = 4;
-		int num2 = 2;
+		float num1 = 4(f);
+		float num2 = 2(f);
 		System.out.println(test.add(num1, num2));
 		System.out.println(test.minus(num1, num2));
 	}
